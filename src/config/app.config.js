f17487ab44272f92110293b179920af309a69c71
anandureghu <anandukr145@gmail.com>
2023-12-07 08:23:28 +0530

    updated config

--- src/config/app.config.js
+++ src/config/app.config.js
@@ -1,21 +1,6 @@
-const {
-  PORT,
-  NODE_ENV,
-  CLIENT_ID,
-  CLIENT_SECRET,
-  REDIRECT_URI,
-  REFRESH_TOKEN,
-  SENDER_EMAIL,
-  SENDER_NAME
-} = process.env;
+const { PORT, NODE_ENV } = process.env;
 
 module.exports = {
   port: PORT || 3000,
   env: NODE_ENV,
-  clientId: CLIENT_ID,
-  clientSecret: CLIENT_SECRET,
-  redirectUri: REDIRECT_URI,
-  refreshToken: REFRESH_TOKEN,
-  senderEmail: SENDER_EMAIL,
-  senderName: SENDER_NAME
 };
--- src/config/mail.config.js
+++ src/config/mail.config.js
@@ -2,8 +2,5 @@ const { FROM, TO, NAME, SUBJECT, PASS } = process.env;
 
 module.exports = {
   from: FROM,
-  to: TO,
-  name: NAME,
-  subject: SUBJECT,
   password: PASS,
 };
